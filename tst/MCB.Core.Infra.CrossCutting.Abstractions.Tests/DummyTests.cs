84b741bd4aed7aac2221a5e8b444c09142139c40
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-11-04 00:05:55 -0300

    test: add dummy test

---
+++ tst/MCB.Core.Infra.CrossCutting.Abstractions.Tests/DummyTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace MCB.Core.Infra.CrossCutting.Abstractions.Tests;
+
+public class DummyTests
+{
+    [Fact]
+    public void DummyTest()
+    {
+        Assert.True(true);
+    }
+}
